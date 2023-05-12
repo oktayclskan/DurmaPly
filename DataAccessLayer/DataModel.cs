e16abe32382f0b4158b577abbf203ea283a39ece
Oktay ÇALIŞKAN <tevizyon@outlook.com>
2023-05-12 22:23:19 +0300

    ..

---
+++ DataAccessLayer/DataModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DataAccessLayer
+{
+    public class DataModel
+    {
+        SqlConnection con; SqlCommand cmd;
+        public DataModel()
+        {
+            con = new SqlConnection(ConnectionString.Constr);
+            cmd = con.CreateCommand();
+        }
+        #region Admin Metots
+
+        #endregion
+        #region Member Metots
+
+        #endregion
+        #region Games Metots
+
+        #endregion
+        #region News Metots
+
+        #endregion
+    }
+}
